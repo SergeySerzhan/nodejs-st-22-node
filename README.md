6b2ff1f748082f8a39227c1c990c5c03da68496b
Sergey Serzhan <serzhansergey96@gmail.com>
2022-07-14 11:01:54 +0300

    feat: complete task-1.3

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # nodejs-st-22-node
+
+## BUILD
+To build scripts using [babel](https://babeljs.io/) you can run
+
+```bash
+npm run build:task-1
+npm run build:task-2
+```
+
+It is create dist folder contains new build scripts.
+
+## RUN
+You can run scripts using [nodemon](https://www.npmjs.com/package/nodemon) with these commands:
+
+```bash
+npm run start:task-1
+npm run start:task-2
+```
---
+++ src/task-1.2.js
@@ -0,0 +1,37 @@
+import csv from 'csvtojson';
+import { createReadStream, createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { pipeline } from 'stream';
+import * as url from 'url';
+
+const __dirname = dirname(url.fileURLToPath(import.meta.url));
+const FILENAME = 'nodejs-hw1-ex1';
+const COLUMNS_WITH_NUMBERS = ['amount', 'price'];
+
+try {
+  const readCsvStream = createReadStream(join(__dirname, `../csv/${FILENAME}.csv`), 'utf-8');
+  const writeTxtStream = createWriteStream(join(__dirname, `../${FILENAME}.txt`));
+
+  const colParser = {};
+  COLUMNS_WITH_NUMBERS.forEach(col => colParser[col] = 'number');
+
+// options to format number data to number type
+  const csvOptions = {
+    colParser,
+    checkType: true
+  };
+
+  pipeline(readCsvStream,
+    csv(csvOptions).preFileLine((line, i) => {
+      // modify columns names to lower case
+      if (i === 0) return line.toLowerCase();
+      else return line;
+    }),
+    writeTxtStream,
+    (err) => {
+      if (err) throw err;
+      console.log('Txt file was successfully created');
+    });
+} catch (err) {
+  console.error(err);
+}
